b93beeb66b6efb1dbf0ec9939006cc18696b40b9
nineleaps <shreya.sisodia@nineleaps.com>
2021-04-30 11:15:34 +0530

    firstcommit

---
+++ src/homepage.component.js
@@ -0,0 +1,41 @@
+import React from "react"; //first import statement for any component you write
+
+const Homepage = () => (
+  <div className="home">
+    this is from home page
+    <div className="maincontainer">
+      <div className="imageContainer">image container</div>
+      <div className="imageContainer">
+        <div className="title">FLOWERS</div>
+        <span className="desc"></span>
+        image container
+      </div>
+      <div className="imageContainer">image container</div>
+      <div className="imageContainer">
+        <div className="title">FLOWERS</div>
+        <span className="desc"></span>
+        image container
+      </div>
+      <div className="imageContainer">image container</div>
+      <div className="imageContainer">
+        <div className="title">FLOWERS</div>
+        <span className="desc"></span>
+        image container
+      </div>
+      <div className="imageContainer">image container</div>
+      <div className="imageContainer">
+        <div className="title">FLOWERS</div>
+        <span className="desc"></span>
+        image container
+      </div>
+      <div className="imageContainer">image container</div>
+      <div className="imageContainer">
+        <div className="title">FLOWERS</div>
+        <span className="desc"></span>
+        image container
+      </div>
+    </div>
+  </div>
+);
+export default Homepage;
+//export default <component name>
